8d467abece4168bf335024a63e2776d351bc2f70
Daniel Rendon <daniel.rendonfc@gmail.com>
2016-02-11 18:25:27 -0600

    added MONK(mongodbtool), ANGULAR and MONGO  dependecies to the app

--- bs/app.js
+++ bs/app.js
@@ -11,7 +11,7 @@ var base = require('./routes/base');
 
 var app = express();
 
-
+// app.locals.delimiters = '<% %>';
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
